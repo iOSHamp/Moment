e46516a2d5314202cec03b43bb1ed2047c10270d
Kayle <kwak8014@kakao.com>
2023-03-28 22:38:52 +0900

    :lipstick: :: 카드 플립

--- Moment.swiftpm/Source/View/ContentView.swift
+++ Moment.swiftpm/Source/View/ContentView.swift
@@ -45,19 +45,81 @@ struct DeckOfCards : View {
     
 }
 
+struct FrontCard: View {
+    var title:String
+    @Binding var degree : Double
+    
+    var body:some View {
+        
+        Image(title)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .rotation3DEffect(Angle(degrees: degree), axis: (x: 0, y: 1, z: 0))
+        
+    }
+    
+}
+
+struct BackCard: View {
+    let back:String = "backCard"
+    let random:Int = Int.random(in: 0...2)
+    @Binding var degree : Double
+    
+    var body:some View {
+        
+        Image("\(back)\(random)")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .rotation3DEffect(Angle(degrees: degree), axis: (x: 0, y: 1, z: 0))
+        
+    }
+    
+    
+}
+
 struct Card : View {
     
-    var title:String
-    let back:String = "backCard"
+    @State var backDegree = 0.0
+    @State var frontDegree = -90.0
+    @State var isFlipped = false
     
-    @State var isFlip:Bool = false
+    
+    let durationAndDelay : CGFloat = 0.2
+    var title:String
+    
+    
+    func flipCard () {
+          isFlipped = !isFlipped
+          if isFlipped {
+              withAnimation(.linear(duration: durationAndDelay)) {
+                  backDegree = 90
+              }
+              withAnimation(.linear(duration: durationAndDelay).delay(durationAndDelay)){
+                  frontDegree = 0
+              }
+          } else {
+              withAnimation(.linear(duration: durationAndDelay)) {
+                  frontDegree = -90
+              }
+              withAnimation(.linear(duration: durationAndDelay).delay(durationAndDelay)){
+                  backDegree = 0
+              }
+          }
+      }
+    
     
     var body: some View {
         
         ZStack{
-            Image(title)
-            Image()
-            
+
+            FrontCard(title: title, degree: $frontDegree)
+            BackCard(degree: $backDegree)
+           
+
+        }
+        .frame(width:UIScreen.height/3,height:UIScreen.height)
+        .onTapGesture {
+            flipCard()
         }
         
         
