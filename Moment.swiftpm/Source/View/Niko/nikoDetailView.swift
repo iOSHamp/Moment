1b807e62559bb794f8af1b4cb115b93f8ae63456
Niko <yejinms@naver.com>
2023-03-28 18:29:50 +0900

    💄니코 디테일 뷰 추가
    니코의 브랜치를 따서 디테일뷰도 새로 만들어 넣었음

---
+++ Moment.swiftpm/Source/View/Niko/nikoDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Niko Yejin Kim on 2023/03/28.
+//
+
+import SwiftUI
+
+struct nikoDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct nikoDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        nikoDetailView()
+    }
+}
