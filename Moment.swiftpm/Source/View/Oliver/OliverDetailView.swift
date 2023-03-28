3df73ddb901597d19d1469abeb44de8e1b1acb2b
Oliver Wonil <stellaria700@gmail.com>
2023-03-28 21:20:13 +0900

    :lipstick: :: 올리버 디테일 뷰 추가
    올리버 디테일 뷰 추가하고 뷰 이름 및 파일 이름 OliverDetailView로 설정

---
+++ Moment.swiftpm/Source/View/Oliver/OliverDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Wonil Lee on 2023/03/28.
+//
+
+import SwiftUI
+
+struct OliverDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OliverDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+		OliverDetailView()
+    }
+}
