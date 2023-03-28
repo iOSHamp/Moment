89dc5f871d06e2aa9414cdc3a3c499bda3c7af80
Kayle <kwak8014@kakao.com>
2023-03-28 11:47:38 +0900

    :lipstick: :: 사진 겹치기 효과

--- Moment.swiftpm/Source/View/ContentView.swift
+++ Moment.swiftpm/Source/View/ContentView.swift
@@ -1,17 +1,68 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var flip:Bool = false
+    @State var opacity:CGFloat = .zero
+    @State var backopacity:CGFloat = 1
+    
+    
     var body: some View {
         ZStack {
-            
-            Color.black
+           
+            DeckOfCards()
+           
                 
         }
-        .padding(.top,safeArea.top)
         .ignoresSafeArea()
         
         
     }
+}
+
+
+struct DeckOfCards : View {
+    
+    let limitOfCard:Int = 8
+    let offset:Double = 2
+    
+    var body: some View {
+        
+        ZStack{
+            
+            ForEach(0..<limitOfCard, id: \.self) { index in
+                
+                Card(title:"mainCard\(index+1)")
+                    .rotationEffect(Angle(degrees: offset * Double(index) ))
+            }
+            
+            
+            
+            
+        }
+        .preferredColorScheme(.dark)
+    }
+    
+}
+
+struct Card : View {
+    
+    var title:String
+
+    @State var isFlip:Bool = false
+    
+    var body: some View {
+        
+        ZStack{
+            
+            
+        }
+        
+        Image(title)
+        
+        
+    }
+    
 }
 
 
