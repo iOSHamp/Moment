5db1c7fec07c79a78d7e68fa30e7fb29ac62385d
Kiyoung <rlaky45@naver.com>
2023-03-28 21:01:01 +0900

    :lipstick: :truck: :: 기영 디테일 뷰 작성 및 리소스 추가

--- Moment.swiftpm/Source/View/Kiyoung/KiyoungDetailView.swift
+++ Moment.swiftpm/Source/View/Kiyoung/KiyoungDetailView.swift
@@ -8,11 +8,55 @@
 import SwiftUI
 
 struct KiyoungDetailView: View {
+    @State var offsetY: CGFloat = 100
+    @State var danceOff: CGFloat = 250
+    @State var isOn = false
     var body: some View {
+        
+        ZStack{
+            Text("We are Pierce!")
+                .offset(y:-120)
+                .font(.largeTitle)
+               
+            Image("presentation")
+                .resizable()
+                .frame(width: 300, height: 180)
+                .cornerRadius(15)
+                .overlay(RoundedRectangle(cornerRadius: 15)
+                            .stroke(Color.black, lineWidth: 3))
+                .shadow(color: .gray, radius: 15,x:10, y: 20)
+                .offset(y:0)
+            HStack{
+                Image("oliver").offset(y:offsetY+30)
+                Image("clau").offset(y:offsetY+20)
+                Image("puppy").offset(y:offsetY)
+                Image("kiyoung").offset(y:offsetY)
+                Image("niko").offset(y:offsetY)
+                Image("kayle").offset(y:offsetY)
+                
+            }
+            
+           
+            Image("people")
+                .offset(x:50 ,y: isOn ? 230 : danceOff)
+                .onTapGesture {
+                    isOn = !isOn
+                }
+                .animation(.default.repeatCount(5), value: isOn)
+                
+            Image("people")
+                .offset(x:-50 ,y: isOn ? 230 : danceOff)
+                .onTapGesture {
+                    isOn = !isOn
+                }
+                .animation(.default.repeatCount(5), value: isOn)
 
+}
         
-        
+
+
     }
+
 }
 
 
