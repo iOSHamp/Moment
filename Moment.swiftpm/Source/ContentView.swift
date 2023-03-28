ed8d2c523f0f6a817b377b4d7e616617ed68b47c
Kayle <kwak8014@kakao.com>
2023-03-28 09:37:41 +0900

    :truck: :: 임시 이미지 및 색깔 추가

---
+++ Moment.swiftpm/Source/ContentView.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            
+            Color.black
+                
+        }
+        .padding(.top,safeArea.top)
+        .ignoresSafeArea()
+        
+        
+    }
+}
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+       ContentView()
+    }
+}
---
+++ Moment.swiftpm/Source/Util/Extension/Extension+Color.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by yongbeomkwak on 2023/03/28.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    
+    static let bgColor:Color = Color("BgColor")
+    
+    
+}
