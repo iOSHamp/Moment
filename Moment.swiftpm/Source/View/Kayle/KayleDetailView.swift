a4cbd49733d36de5c64f780ac3d5658c92e2d1ee
Kayle <kwak8014@kakao.com>
2023-03-28 09:42:29 +0900

    :lipstick: :: 카일 디테일 뷰 생성

---
+++ Moment.swiftpm/Source/View/Kayle/KayleDetailView.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+
+struct KayleDetailView: View {
+    var body: some View {
+        ZStack {
+            
+            Color.red
+                
+        }
+        .padding(.top,safeArea.top)
+        .ignoresSafeArea()
+        
+        
+    }
+}
+
+
+
+struct KayleDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        KayleDetailView()
+    }
+}
