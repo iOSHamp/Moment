1cef069099dd26c6bbf24a46ec7a6681368f1533
Jinyoung (Claudia) Park <jyp.dsgn@gmail.com>
2023-03-29 12:41:34 +0900

    :lipstick: :: 끌라우 디테일뷰 수정

--- Moment.swiftpm/Source/View/Claudia/claudiaDetailView.swift
+++ Moment.swiftpm/Source/View/Claudia/claudiaDetailView.swift
@@ -9,7 +9,35 @@ import SwiftUI
 
 struct SwiftUIView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            Color(hex: 0xF9F5EA)
+                .ignoresSafeArea()
+            
+            VStack {
+                Image("mainCard2")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width:300)
+                Text("Push here👇")
+                    .font(.body)
+                    .foregroundColor(Color(hue: 0.388, saturation: 0.764, brightness: 0.494))
+                Button {
+                    print("¡Soy Claudia!")
+                } label: {
+                    Text("¡Hola!")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+                        .padding()
+                        .frame(width: 230)
+                        .background(Color(red: 0.21568627450980393, green: 0.5882352941176471, blue: 0.43529411764705883))
+                        .cornerRadius(20)
+                }
+            }
+        }
+        
+
     }
 }
 
