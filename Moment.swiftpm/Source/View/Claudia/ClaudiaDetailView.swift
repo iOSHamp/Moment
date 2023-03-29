b83702d9c9b731da60dfd705b503d6c4db0d052f
Jinyoung (Claudia) Park <jyp.dsgn@gmail.com>
2023-03-29 20:57:14 +0900

    :lipstick: :truck: :: 디테일뷰 수정 및 이미지 추가
    디테일뷰 작업 및 스티커 이미지 추가 했습니다.

--- Moment.swiftpm/Source/View/Claudia/ClaudiaDetailView.swift
+++ Moment.swiftpm/Source/View/Claudia/ClaudiaDetailView.swift
@@ -15,25 +15,70 @@ struct ClaudiaDetailView: View {
                 .ignoresSafeArea()
             
             VStack {
-                Image("mainCard2")
+                Image("mainCard1")
                     .resizable()
                     .scaledToFit()
                     .frame(width:300)
-                Text("Push here👇")
+                    .padding(.bottom, 40)
+                    .shadow(color: Color(hex: 0xCEBEA4, alpha: 1), radius: 5, x: 2, y: 4) //Shadow layer 1
+                    .shadow(color: Color(hex: 0xCEBEA4, alpha: 0.5), radius: 5, x: 9, y: 9) //Shadow layer 2
+                Text("Tap here👇")
                     .font(.body)
                     .foregroundColor(Color(hue: 0.388, saturation: 0.764, brightness: 0.494))
                 Button {
                     print("¡Soy Claudia!")
                 } label: {
-                    Text("¡Hola!")
-                        .font(.largeTitle)
+                    Text("🚢 포항항ꉂꉂ(ᵔᗜᵔ*) 💙")
+                        .font(.title3)
                         .fontWeight(.bold)
                         .foregroundColor(Color.white)
                         .padding()
                         .frame(width: 230)
-                        .background(Color(red: 0.21568627450980393, green: 0.5882352941176471, blue: 0.43529411764705883))
+                        .background(Color(hex: 0x2F53A1))
                         .cornerRadius(20)
+                        .shadow(color: Color(hex: 0xCEBEA4, alpha: 1), radius: 5, x: 2, y: 4) //Shadow layer 1
+                        .shadow(color: Color(hex: 0xCEBEA4, alpha: 0.5), radius: 5, x: 9, y: 9) //Shadow layer 2
+                    
+                    
+                    }
                 }
+            ZStack {
+                Image("clauSticker")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width:130)
+                    .position(x: 355, y: 180)
+                    .rotationEffect(.degrees(-10))
+                Image("kayleSticker")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width:100)
+                    .position(x: 85, y: 480)
+                    .rotationEffect(.degrees(10))
+                Image("oliverSticker")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width:70)
+                    .position(x: 260, y: 110)
+                    .rotationEffect(.degrees(-10))
+                Image("nikoSticker")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width:110)
+                    .position(x: 330, y: 370)
+                    .rotationEffect(.degrees(10))
+                Image("puppySticker")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width:130)
+                    .position(x: 140, y: 75)
+                    .rotationEffect(.degrees(-10))
+                Image("kiyoungSticker")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width:130)
+                    .position(x: 280, y: 500)
+                    .rotationEffect(.degrees(10))
             }
         }
         
