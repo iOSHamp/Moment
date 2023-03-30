076d00cf1f4fc121e1b71358437a1362140a17b8
Jinyoung (Claudia) Park <jyp.dsgn@gmail.com>
2023-03-30 10:07:29 +0900

    :lipstick: :: 끌라우 디테일뷰 수정
    폭죽 효과를 추가했습니다.

--- Moment.swiftpm/Source/View/Claudia/ClaudiaDetailView.swift
+++ Moment.swiftpm/Source/View/Claudia/ClaudiaDetailView.swift
@@ -6,8 +6,13 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct ClaudiaDetailView: View {
+    @State var 포항항 = false
+    // Finish 포항항
+    @State var finish포항항 = false
+    
     var body: some View {
         
         ZStack {
@@ -25,9 +30,8 @@ struct ClaudiaDetailView: View {
                 Text("Tap here👇")
                     .font(.body)
                     .foregroundColor(Color(hue: 0.388, saturation: 0.764, brightness: 0.494))
-                Button {
-                    print("¡Soy Claudia!")
-                } label: {
+                
+                Button (action: doAnimation, label: {
                     Text("🚢 포항항ꉂꉂ(ᵔᗜᵔ*) 💙")
                         .font(.title3)
                         .fontWeight(.bold)
@@ -40,8 +44,11 @@ struct ClaudiaDetailView: View {
                         .shadow(color: Color(hex: 0xCEBEA4, alpha: 0.5), radius: 5, x: 9, y: 9) //Shadow layer 2
                     
                     
-                    }
+                    })
+                .disabled(포항항)
+                
                 }
+            
             ZStack {
                 Image("clauSticker")
                     .resizable()
@@ -80,14 +87,123 @@ struct ClaudiaDetailView: View {
                     .position(x: 280, y: 500)
                     .rotationEffect(.degrees(10))
             }
+            
+            EmitterView()
+                .scaleEffect(포항항 ? 1 : 0, anchor: .top)
+                .opacity(포항항 && !finish포항항 ? 1 : 0)
+                .offset(y: 포항항 ? 0 : UIScreen.height / 2)
+                .ignoresSafeArea()
+        }
+    }
+    
+    func doAnimation(){
+        
+        withAnimation(.spring()){
+            
+            포항항 = true
         }
         
-
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            
+            withAnimation(.easeInOut(duration: 1.5)){
+                
+                finish포항항 = true
+            }
+            // Reseting After 포항항 Finished
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.4){
+                
+                finish포항항 = false
+                포항항 = false
+            }
+            
+        }
     }
 }
+
+struct EmitterView: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> UIView {
+        
+        let view = UIView()
+        view.backgroundColor = .clear
+        
+        // Emitter Layer
+        let emitterLayer = CAEmitterLayer()
+        emitterLayer.emitterShape = .line
+        emitterLayer.emitterCells = createEmiterCells()
+        
+        //Size and Position
+        emitterLayer.emitterSize = CGSize(width: UIScreen.width, height: 1)
+        emitterLayer.emitterPosition = CGPoint(x: UIScreen.width / 2, y: 0)
+        
+        view.layer.addSublayer(emitterLayer)
+        
+        
+        return view
+    }
+    func updateUIView(_ uiView: UIView, context: Context) {
+        
+    }
+    
+    func createEmiterCells() -> [CAEmitterCell] {
+        
+        // Multiple Emmiters
+        
+        var emitterCells: [CAEmitterCell] = []
+        
+        for index in 1...12{
+            
+            let cell = CAEmitterCell()
+            
+            cell.contents = UIImage(named: getImage(index: index))?.cgImage
+            cell.color = getColor().cgColor
+            // New Particle Creation
+            cell.birthRate = 4.5
+            // Particle Existence
+            cell.lifetime = 20
+            // velocity
+            cell.velocity = 120
+            // Scale
+            cell.scale = 0.05
+            cell.scaleRange = 0.2
+            cell.emissionLongitude = .pi
+            cell.emissionRange = 0.5
+            cell.spin = 3.5
+            cell.spinRange = 1
+            
+            // Acceleration
+            cell.yAcceleration = 40
+            
+            emitterCells.append(cell)
+        }
+        
+        return emitterCells
+    }
+    func getColor()->UIColor{
+        let colors : [UIColor] = [.systemOrange, .systemCyan, .systemPink, .systemTeal]
+        
+        return colors.randomElement()!
+    }
+    
+    func getImage(index: Int)->String{
+        
+        if index < 4{
+            return "square"
+        }
+        else if index > 5 && index <= 8{
+            return "circle"
+        }
+        else{
+            return"star"
+        }
+    }
+}
+
+
 
 struct ClaudiaDetailView_Previews: PreviewProvider {
     static var previews: some View {
         ClaudiaDetailView()
     }
 }
+
